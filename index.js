4d7a7653faf297893324a53f92af1947fcd967ce
Quadri Usman <tqu14ee1065@gmail.com>
2022-12-12 14:36:53 +0100

    edited

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ app.use(indexRoutes);
 // ===============
 
 // Listen Port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8081;
 app.listen(PORT, () =>
   console.log("Server is running at http://127.0.0.1:%s", `${PORT}`)
 );
